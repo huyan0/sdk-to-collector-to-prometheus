9d4f37defb472543148c42132e58a49cd6283a6d
Yang Hu <yhuz@amazon.com>
2020-07-23 10:00:08 -0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go.opentelemetry.io
+
+go 1.14
+
+require (
+	go.opentelemetry.io/otel v0.9.0
+	go.opentelemetry.io/otel/exporters/metric/prometheus v0.9.0
+	go.opentelemetry.io/otel/exporters/otlp v0.9.0
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/metric"
+	"go.opentelemetry.io/otel/exporters/otlp"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+)
+
+func main() {
+
+	exporter, err := otlp.NewExporter(
+		otlp.WithInsecure()) // Configure as needed.
+	if err != nil {
+		log.Fatalf("failed to create exporter: %v", err)
+	}
+	defer func() {
+		err := exporter.Stop()
+		if err != nil {
+			log.Fatalf("failed to stop exporter: %v", err)
+		}
+	}()
+
+	pusher := push.New(simple.NewWithInexpensiveDistribution(), exporter, push.WithPeriod(1*time.Second))
+
+	pusher.Start()
+
+	meter := pusher.Provider().Meter("example")
+	ctx := context.Background()
+
+	// Create two instruments with Go SDK metric package
+	counter0 := metric.Must(meter).NewInt64Counter(
+		"a_counter_i",
+		metric.WithDescription("Adds i every time, growth should be O(i!)"),
+	)
+	counter1 := metric.Must(meter).NewInt64Counter(
+		"a_counter_1",
+		metric.WithDescription("Adds 1 every time, growth should be O(i)"),
+	)
+
+	recorder := metric.Must(meter).NewInt64ValueRecorder(
+		"a_valuerecorder",
+		metric.WithDescription("Records values"),
+	)
+
+	// Add initial values to the instruments
+	counter1.Add(ctx, 0, kv.String("kind", "counter"))
+	counter0.Add(ctx, 0, kv.String("kind", "counter"))
+	recorder.Record(ctx, 0, kv.String("kind", "gauge"))
+	var wg sync.WaitGroup
+	wg.Add(1)
+	value := 0
+	// Repeatedly record values every 3 seconds
+	go func() {
+		for i := 1; i <= 100; i++ {
+			time.Sleep(1 * time.Second)
+			value += i
+			fmt.Printf("%d. Recording %d in gauge and adding 1 to counter, counter0 should be %d, counter1 should %d \n", i, i, i, value)
+			recorder.Record(ctx, int64(i), kv.String("kind", "gauge"))
+			counter0.Add(ctx, int64(i), kv.String("kind", "counter"))
+			counter1.Add(ctx, int64(1), kv.String("kind", "counter"))
+		}
+	}()
+	wg.Wait()
+}
